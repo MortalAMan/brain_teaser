53ae4de083d6dc6a3dc0f28a08821631e9cd5c52
Addy King <ashepherdking@gmail.com>
2017-07-21 10:40:35 -0600

    brain_teaser

---
+++ brain_teas.rb
@@ -0,0 +1,27 @@
+str = "one two three"
+arr = str.split
+counter = 0
+most_repeat_letter_word = nil
+
+def candidate
+  candidate = w.length - w.split('').uniq.length
+if candidate  > counter 
+    most_repeat_letter_word = w
+    counter = candidate
+  end
+end
+puts most_repeat_letter_word
+
+
+ 
+  
+
+
+
+
+
+
+
+
+  
+ 
